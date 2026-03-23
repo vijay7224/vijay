695e40ef4e2dc82c81420ec2705c6ac0d1ee991d
vijay7224 <vijaysuryawanshi7224@gmail.com>
2026-03-23 16:57:47 +0530

    Update app.py

--- stockmodel/app.py
+++ stockmodel/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def app():
+def home():   # ✅ function name change kiya
     return render_template('index.html')
 
 if __name__ == "__main__":
