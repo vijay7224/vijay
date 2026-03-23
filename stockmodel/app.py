2ee9cac044383d4bab1bf5c96478203b6602ba68
vijay7224 <vijaysuryawanshi7224@gmail.com>
2026-03-23 16:56:43 +0530

    Update app.py

--- stockmodel/app.py
+++ stockmodel/app.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 def app():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
